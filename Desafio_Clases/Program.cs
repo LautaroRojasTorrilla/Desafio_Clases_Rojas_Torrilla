6e653b45cf03aa718f990263468868528f15dfa2
LautaroRojasTorrilla <lautaro.rojas.torrilla@gmail.com>
2024-01-16 21:39:58 -0300

    entrega

---
+++ Desafio_Clases/Program.cs
@@ -0,0 +1,27 @@
+﻿using Models;
+
+namespace Desafio_Clases
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Crear un objeto Producto
+            Producto miProducto = new Producto(1, "Laptop", 800, 1200, 10, 123);
+
+            // Mostrar los datos del producto
+            Console.WriteLine("Datos del Producto:\n");
+            Console.WriteLine(miProducto.MostrarDatos());
+
+            // Modificar algunos valores
+            miProducto.Stock = 8;
+            miProducto.PrecioVenta = 1300;
+
+            // Mostrar los datos actualizados
+            Console.WriteLine("Datos del Producto Actualizados:\n");
+            Console.WriteLine(miProducto.MostrarDatos());
+
+            Console.ReadKey();
+        }
+    }
+}
